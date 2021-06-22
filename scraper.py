602b9074079e49825da6ad582f22e900dd127644
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-06-22 14:52:23 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,11 +2,14 @@ import subprocess,sys, re
 
 count = 0
 file1 = open('URLList.txt')
+f2 = open('defaultBrowser.txt') #Refactor to make file (xdg-settings get default-web-browser >> *WHATEVERFILENAME*)
+browser = f2.readline()
+f2.close()
 while True:
     count+=1
     URL = file1.readline()
     if re.search('www.bestbuy.com/',URL):
-        subprocess.run(["python3","getBB.py",URL])
+        subprocess.run(["python3","getBB.py",URL,browser])
 
     if not URL:
         file1.close()
