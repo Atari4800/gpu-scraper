b3bd1e9498095c5ba412ec7cf3baf1c2662ff3d2
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-06-22 18:33:16 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,16 +1,12 @@
-import subprocess,sys, re
+import subprocess,sys, re,json
 
+with open("productList.json","r") as read_file:
+    data = json.load(read_file)
 count = 0
 file1 = open('URLList.txt')
 f2 = open('defaultBrowser.txt') #Refactor to make file (xdg-settings get default-web-browser >> *WHATEVERFILENAME*)
 browser = f2.readline()
 f2.close()
-while True:
-    count+=1
-    URL = file1.readline()
-    if re.search('www.bestbuy.com/',URL):
-        subprocess.run(["python3","getBB.py",URL,browser])
-
-    if not URL:
-        file1.close()
-        break
+for URL in data['Product']:
+    print(URL)
+    subprocess.Popen(["python3","getBB.py",URL['productLink'],browser])
