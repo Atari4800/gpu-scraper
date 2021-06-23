3256771c52a1f5c1f579fa2c4e56bbb750a143ab
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-06-22 19:24:40 -0500

    Create getBH.py

---
+++ getBH.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from bs4 import BeautifulSoup
+import requests, sys, webbrowser,re
+
+URL=sys.argv[1]                                                                  
+
+
+option = Options()
+option.headless = True
+browser=webdriver.Firefox(options=option, executable_path = './drivers/geckodriver')
+browser.get(URL)
+soup = BeautifulSoup(browser.page_source,'html.parser')
+results = soup.find(class_='cartRow_2dS2mdogHYAqhmKoANr6Ol')
+if re.search("Add to Cart",str(results)):
+    webbrowser.open_new(URL)
+browser.close()
