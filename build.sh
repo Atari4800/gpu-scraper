cd3db78ea710e8ba6825a92e0e7373372106d334
Atari4800 <jmcmaken@unomaha.edu>
2021-06-22 19:50:39 -0500

    config needs to have default browser in it

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ pip3 install tkinter
 pip3 install selenium
 apt-get install python3-bs4
 pip3 install python-crontab
+xdg-settings get default-web-browser > defaultBrowser.txt 
 cd gpu-hunter
 echo "build complete"
