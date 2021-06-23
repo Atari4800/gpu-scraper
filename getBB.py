bf4368505d09f9b845298e012fe217e936ab08f1
Atari4800 <jmcmaken@unomaha.edu>
2021-06-22 20:15:05 -0500

    Fix formatting

--- getBB.py
+++ getBB.py
@@ -1,23 +1,26 @@
+import requests
+import sys
+import webbrowser
+import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
-import requests, sys, webbrowser,re
 
-URL=sys.argv[1]
+URL = sys.argv[1]
 
 option = Options()
 option.headless = True
-browser=webdriver.Firefox(options=option, executable_path = './drivers/geckodriver')
+browser = webdriver.Firefox(options = option, executable_path = './drivers/geckodriver')
 browser.get(URL)
 
-with open('bboutput.html','wt',encoding='utf-8') as f:
+with open('bboutput.html','wt', encoding = 'utf-8') as f:
     f.write(browser.page_source)
 
-soup = BeautifulSoup(browser.page_source,'html.parser')
-results = soup.find(class_='fulfillment-add-to-cart-button')
+soup = BeautifulSoup(browser.page_source, 'html.parser')
+results = soup.find(class_ = fulfillment-add-to-cart-button')
 
-if re.search("Add to Cart",str(results)):
+if re.search("Add to Cart", str(results)):
     webbrowser.open_new(URL)
 
 browser.close()
--- getNE.py
+++ getNE.py
@@ -1,23 +1,26 @@
+import requests
+import webbrowser
+import sys
+import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
-import requests, sys, webbrowser,re
 
-URL=sys.argv[1]
+URL = sys.argv[1]
 
 option = Options()
 option.headless = True
-browser=webdriver.Firefox(options=option, executable_path = './drivers/geckodriver')
+browser = webdriver.Firefox(options = option, executable_path = './drivers/geckodriver')
 browser.get(URL)
 
-with open('neoutput.html','wt',encoding='utf-8') as f:
+with open('neoutput.html','wt',encoding = 'utf-8') as f:
     f.write(browser.page_source)
 
-results = soup.find(id='ProductBuy')
-themessage = results.find_all('button',class_='btn btn-primary btn-wide')
+results = soup.find(id = 'ProductBuy')
+themessage = results.find_all('button', class_ = 'btn btn-primary btn-wide')
 
-if re.search("Add",themessage):
+if re.search("Add", themessage):
     webbrowser.open_new(URL)
 #for themessage in themessage:
 #    text_elem = themessage.text
