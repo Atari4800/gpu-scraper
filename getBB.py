dff337d0c1022cc9837fbfd0064a9c770f265a6d
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-06-23 13:11:51 -0500

    Update getBB.py

--- getBB.py
+++ getBB.py
@@ -5,18 +5,18 @@ import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options as FFOpt
-from selenium.webdriver.chrome.options import Options as COpt
+#from selenium.webdriver.chrome.options import Options as COpt
 from bs4 import BeautifulSoup
 
 URL = sys.argv[1]
 theDriver = './drivers/geckodriver'
 option = FFOpt()
-if sys.argv[2] != 'firefox.desktop':
-    option=COpt()
-    theDriver = './drivers/chromedriver'
+#if sys.argv[2] != 'firefox.desktop':
+ #   option=COpt()
+  #  theDriver = './drivers/chromedriver'
 
 option.headless = True
-browser = webdriver.Firefox(options = option, executable_path = theDriver)
+browser = webdriver.Firefox(firefox_options = option, executable_path = theDriver)
 browser.get(URL)
 
 soup = BeautifulSoup(browser.page_source, 'html.parser')
