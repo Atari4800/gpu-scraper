ac5b749eb99f2b6bee40a35352465295752c6be5
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-06-24 14:26:21 -0500

    Update getBH.py
    added an exit so it doesn't hog ALL of the memory.

--- getBH.py
+++ getBH.py
@@ -11,17 +11,20 @@ from bs4 import BeautifulSoup
 URL = sys.argv[1]
 theDriver = './drivers/geckodriver'
 option = FFOpt()
-
+#if sys.argv[2] != 'firefox.desktop':
+#    option=COpt()
+#    theDriver = './drivers/chromedriver'
 
 option.headless = True
 browser = webdriver.Firefox(options = option, executable_path = theDriver)
 browser.get(URL)
 
 soup = BeautifulSoup(browser.page_source,'html.parser')
+
 results = soup.find(class_ = 'cartRow_2dS2mdogHYAqhmKoANr6Ol')
 
 if re.search("Add to Cart",str(results)):
     webbrowser.open_new(URL)
-    
-browser.close()
 
+
+exit()
