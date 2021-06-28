74ea86ace939c3425aa2f849d8c9731c1bac794a
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-06-28 13:09:36 -0500

    Update scheduler.py
    Now takes the minutes as an argument
    ie) 
    "python3 ./scheduler.py 12"
    Will run the cron-job every 12 minutes. Also re-do's the program' created Cron-job instead of creating multiple cron-jobs every time the program is run.

--- scheduler.py
+++ scheduler.py
@@ -1,21 +1,29 @@
-import os
+import os, sys
 from crontab import CronTab
 
 class Scheduler:
     def __init__(self,minutes):
+#        if(cron.find_command() > 0)
+
         self.minute=minutes
         self.cron = CronTab(user=True)
+        basicIter = cron.find_comment('Search for GPU task')
+        num=0
+        for item in basicIter:
+            num=num+1
+            self.cron.remove(item)
         currdir=str(os.getcwd())
         com = 'export DISPLAY=:0 && cd ' + currdir + ' && python3 scraper.py'
         self.job = self.cron.new(command = com)
-#'export DISPLAY=:0 && cd ~/Desktop/gpu-scraper-Harrison-Frame-codeSample/ && ls -l && python3 scraper.py')
- #       self.job = self.cron.new(command = 'echo "Eat" >> diditgo.txt') #This is a test-line for the jobs.
+        self.job.set_comment('Search for GPU task')
+        print('CRON-JOB INITIATED FOR '+minutes+ ' MINUTES')
         self.job.minute.every(self.minute)
-        print(self.cron.write())
+        self.cron.write()
     def ChangeMinutes(min):
         self.minutes=min
-    def check():
-        for jobs in self.cron:
-            print(jobs)
-sc = Scheduler(1)
-sc.check
+
+
+cron = CronTab(user = True)
+
+sc = Scheduler(sys.argv[1])
+exit()
