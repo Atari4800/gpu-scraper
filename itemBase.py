b4f9ebd7480c1a3162330a7e3562235ddd98e6ca
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-07-02 11:21:11 -0500

    Update and rename productAdder.py to itemBase.py
    Renamed productAdder.py to itemBase.py.
    Refactored so now it's a class. Functionality is still the same.

---
+++ itemBase.py
@@ -0,0 +1,88 @@
+import json, sys, re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options as FFOpt
+from bs4 import BeautifulSoup
+
+
+
+
+class itemBase:
+    def addItem(self, URL):
+        with open("productList.json", "r") as dataFile:
+            data = json.load(dataFile)
+
+        for URLStr in data['Product'] :
+            if str(URLStr['productLink']) == URL :
+                print("Duplicate URL, cannot add.")
+                exit(1)
+        print("Adding URL...")
+
+        theDriver = './drivers/geckodriver'
+        option = FFOpt()
+        option.headless = True
+        browser = webdriver.Firefox(options = option, executable_path = theDriver)
+        browser.get(URL)
+        title = ' '
+        price = ' '
+        soup = BeautifulSoup(browser.page_source,'html.parser')
+
+        if re.search("www.bestbuy.com/", URL) :
+            results = soup.find(class_ = 'sku-title')
+            results = results.find(class_ = 'heading-5 v-fw-regular')
+            title=str(results)[len('h1 class = \"heading-5 v-fw-regular\"'):-len('</h1>')]
+            print('Item is called ' +title)
+            results = soup.find(class_ = 'priceView-hero-price priceView-customer-price')
+            priceStr=str(results)
+            price=float(priceStr[priceStr.index('aria-hidden="true">') + len('aria-hidden="true">') +1:priceStr.index('</span><span class')].replace(',',''))
+            print('The price is ' + str(price))
+        elif re.search("www.newegg.com/",URL) :
+            results = soup.find(class_ = 'product-title')
+            title = str(results)[len('<h1 class=\"product-title\">'):-len('</h1>')]
+            print(title)
+            results = soup.find(class_ = 'product-price')
+            priceStr=results.get_text()
+            if re.search('Sale',priceStr) :
+                price = re.findall("\d+\.\d+",priceStr)
+                price = float(price[0])
+                print(str(price))
+            else:
+                price=float(priceStr.split('$')[1].replace(',',''))
+        elif re.search("www.bhphotovideo.com",URL) :
+            results = soup.find(class_ = 'title_1S1JLm7P93Ohi6H_hq7wWh')
+            if results == None :
+                print('B&H bot detection has picked you up. Please increase product checking interval in the scheduler. Then go to B&H\'s website to do their recaptcha and try again.')
+                browser.close()
+                exit()
+            title=results.get_text().split('BH')[0]
+            print(title)
+            results = soup.find(class_ = 'price_1DPoToKrLP8uWvruGqgtaY')
+            priceStr=results.get_text()
+            price = float(priceStr.split('$')[1].replace(',',''))
+            print(price)
+        else:
+            print("The text which was input is not supported")
+            browser.close()
+            exit()
+
+        browser.close()
+        self.addJSON(title, URL, price)
+        print("URL Added Successfully.")
+
+
+
+    def addJSON(self, title, URL, price):
+        with open("productList.json", "r") as dataFile:
+            data = json.load(dataFile)
+        jsonObj = {'productType':title,'productLink':URL,'productPrice':price}
+        data['Product'].append(jsonObj)
+        dataFile = open('productList.json','w+')
+        dataFile.seek(0)
+
+        json.dump(data,dataFile)
+        dataFile.truncate()
+        dataFile.close()
+
+type = sys.argv[1]
+URL = sys.argv[2]
+itemBase().addItem(URL)
