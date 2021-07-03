975e94e7ed58059b9f32687c93595ed3ef2562c5
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-07-03 14:42:50 -0500

    Update initiator.py
    Added class,
    Now includes  a call to interface.notification so the UI generates a notification.

--- initiator.py
+++ initiator.py
@@ -2,7 +2,7 @@ import subprocess
 import re
 import json
 
-# import interface
+import interface
 
 
 class initiator:
@@ -25,28 +25,37 @@ class initiator:
                 self.data = json.load(data_file)
 
         theProcesses=[]
+        theData = []
         for URL in self.data['Product']:
 
             URLstr=str(URL['productLink'])
             print('running for ' + URLstr)
             if re.search("www.bestbuy.com/", URLstr) :
                 theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BB', self.defaultBrowser]))
+                theData.append([URL['productType'],URLstr,URL['productPrice'],False])
             if re.search("www.newegg.com/", URLstr) :
                 theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'NE', self.defaultBrowser]))
+                theData.append([URL['productType'], URLstr, URL['productPrice'], False])
             if re.search("www.bhphotovideo.com/", URLstr) :
                 theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BH', self.defaultBrowser]))
+                theData.append([URL['productType'], URLstr, URL['productPrice'], False])
 
             while len(theProcesses) > 2:
+                count = 0 
                 for p in theProcesses :
                     if p.poll() == 0:
-                        theProcesses.remove(p)
+                        theData.pop(count)
+                        theProcesses.pop(count)
                     elif p.poll() == 1:
-                        theProcesses.remove(p)
-                        # interface.notification()
+                        theProcesses.pop(count)
+                        interface.notification(theData[count][0],theData[count][1],theData[count][2],theData[count][3])
+                        theData.pop(count)
                         print('Interface call!!!!!')
                     elif p.poll() == 2:
-                        theProcesses.remove(p)
+                        theProcesses.pop(count)
+                        theData.pop(count)
                         print('Network Error')
+                    count = count + 1
 
 gotime = initiator()
 gotime.initiate()
