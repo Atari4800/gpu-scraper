342805fc93d518379aebfd232072752d1c436302
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-07-03 13:57:57 -0500

    Update initiator.py
    transformed into a class

--- initiator.py
+++ initiator.py
@@ -1,34 +1,53 @@
 import subprocess
-import sys
 import re
 import json
 
-with open("productList.json", "r") as data_file:
-    data = json.load(data_file)
-
-with open('defaultBrowser.txt') as fp:
-    browser = fp.readline()
-theProcesses=[]
-for URL in data['Product']:
-
-    URLstr=str(URL['productLink'])
-    print('running for ' + URLstr)
-    if re.search("www.bestbuy.com/", URLstr) :
-        theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BB', browser]))
-    if re.search("www.newegg.com/", URLstr) :
-        theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'NE', browser]))
-    if re.search("www.bhphotovideo.com/", URLstr) :
-        theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BH', browser]))
-
-    while len(theProcesses) > 3:
-        for p in theProcesses :
-            if p.poll() == 0:
-                theProcesses.remove(p)
-            elif p.poll() == 1:
-                theProcesses.remove(p)
-                print('Interface call!!!!!')
-            elif p.poll() == 2:
-                print('Network Error')
+# import interface
 
 
+class initiator:
+    def __init__(self):
+        self.prodLink = "productList.json"
+        self.defaultBrowser=''
+        self.data = None
+        print(self.prodLink)
+    def setDefaultBrowser(self):
+        with open('defaultBrowser.txt') as fp:
+            self.defaultBrowser = fp.readline()
+
+
+    def pollSite(baseURL):
+        return(subprocess.run(["ping -c 1 " + baseURL]))
+
+    def initiate(self):
+        print('initiated')
+        with open(self.prodLink, "r") as data_file:
+                self.data = json.load(data_file)
+
+        theProcesses=[]
+        for URL in self.data['Product']:
+
+            URLstr=str(URL['productLink'])
+            print('running for ' + URLstr)
+            if re.search("www.bestbuy.com/", URLstr) :
+                theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BB', self.defaultBrowser]))
+            if re.search("www.newegg.com/", URLstr) :
+                theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'NE', self.defaultBrowser]))
+            if re.search("www.bhphotovideo.com/", URLstr) :
+                theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BH', self.defaultBrowser]))
+
+            while len(theProcesses) > 2:
+                for p in theProcesses :
+                    if p.poll() == 0:
+                        theProcesses.remove(p)
+                    elif p.poll() == 1:
+                        theProcesses.remove(p)
+                        # interface.notification()
+                        print('Interface call!!!!!')
+                    elif p.poll() == 2:
+                        theProcesses.remove(p)
+                        print('Network Error')
+
+gotime = initiator()
+gotime.initiate()
 exit()
