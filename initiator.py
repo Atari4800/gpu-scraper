5602e23ab2c7c4aa1306ea31544ab30d753bc60a
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-07-03 13:10:30 -0500

    Update and rename scraper.py to initiator.py
    Updated to reflect what the script does.  It 'INITIATES' calls to scraper.py. 
    --Needed functionality is to make calls to interface.py based on the p.poll() ==  result. for update.

--- initiator.py
+++ initiator.py
@@ -14,16 +14,21 @@ for URL in data['Product']:
     URLstr=str(URL['productLink'])
     print('running for ' + URLstr)
     if re.search("www.bestbuy.com/", URLstr) :
-        theProcesses.append(subprocess.Popen(["python3", "getBB.py", URL['productLink'], browser]))
+        theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BB', browser]))
     if re.search("www.newegg.com/", URLstr) :
-        theProcesses.append(subprocess.Popen(["python3", "getNE.py", URL['productLink'], browser]))
+        theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'NE', browser]))
     if re.search("www.bhphotovideo.com/", URLstr) :
-        theProcesses.append(subprocess.Popen(["python3", "getBH.py", URL['productLink'], browser]))
+        theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BH', browser]))
 
     while len(theProcesses) > 3:
         for p in theProcesses :
-            if p.poll() != None:
+            if p.poll() == 0:
                 theProcesses.remove(p)
+            elif p.poll() == 1:
+                theProcesses.remove(p)
+                print('Interface call!!!!!')
+            elif p.poll() == 2:
+                print('Network Error')
 
 
 exit()
