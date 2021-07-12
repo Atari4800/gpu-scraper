419d264ad09c96b6d3cf95124616d4501b074e7b
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-07-12 17:19:19 -0500

    Update initiator.py

--- initiator.py
+++ initiator.py
@@ -104,7 +104,7 @@ class initiator:
                         numCrawled = numCrawled + 1
                     elif p.poll() == 1:
                         theProcesses.pop(count)
-                        #interface.notification(theData[count][0],theData[count][1],theData[count][2],theData[count][3])
+                        interface.notification(theData[count][0],theData[count][1],theData[count][2],theData[count][3])
                         theData.pop(count)
                         print('Interface call!!!!!')
                         numCrawled = numCrawled + 1
