2afba7196bf5c3d9340b2e50703a9054e767bd88
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-07-12 18:47:51 -0500

    Update scraper.py
    updated file to pep8, might be unstable. Not all functionality was tested on refactor.

--- scraper.py
+++ scraper.py
@@ -12,91 +12,94 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options as FFOpt
 from bs4 import BeautifulSoup
 
-class scraper :
+
+class scraper:
     """
     This class handles web scraping operations.
     """
-    def __init__(self, URL, uType):
+
+    def __init__(self, url, u_type):
         """
         Instantiates a web scraper and pulls initial page source information.
-        :param URL: The URL of a link that will be scraped to find product information
-        :param uType: The type of link that a URL is
+        :param url: The URL of a link that will be scraped to find product information
+        :param u_type: The type of link that a URL is
                         BB - Bestbuy
                         NE - Newegg
                         BH - B&H
         """
-        if uType != None:
+        if u_type is not None:
             self.theDriver = './drivers/geckodriver'
             self.option = FFOpt()
             self.option.headless = True
             try:
                 self.browser = webdriver.Firefox(options=self.option, executable_path=self.theDriver)
             except:
-                print("Could not find the driver. Ensure that the \'geckodriver\' is in the directory: ./drivers/geckodriver")
+                print(
+                    "Could not find the driver. Ensure that the \'geckodriver\' is in the directory: "
+                    "./drivers/geckodriver")
                 exit(-1)
             try:
-                self.browser.get(URL)
+                self.browser.get(url)
             except:
                 print("Could not obtain browser information. Are you sure that is a Newegg, Bestbuy, or B&H link?")
                 exit(-1)
-            self.soup = BeautifulSoup(self.browser.page_source,'html.parser')
+            self.soup = BeautifulSoup(self.browser.page_source, 'html.parser')
             self.browser.close()
 
-
-
-    def getBB(self):
+    def getbb(self):
         """
-        Searches for whether or not the HTML source grabbed by the scraper contains a product available for purchase with BestBuy
-        :return: 0 if the product is not found available for purchase.
-        :return: 1 if the product is found and available for purchase.
+        Searches for whether or not the HTML source grabbed by the scraper contains a product available for purchase
+        with BestBuy :return: 0 if the product is not found available for purchase. :return: 1 if the product is
+        found and available for purchase.
         """
-        results = self.soup.find(class_ = 'fulfillment-add-to-cart-button')
-        if results != None:
+        results = self.soup.find(class_='fulfillment-add-to-cart-button')
+        if results is not None:
             if re.search("Add to Cart", str(results)):
                 webbrowser.open_new(URL)
                 return 1
         return 0
 
-    def getBH(self):
+    def getbh(self):
         """
-        Searches for whether or not the HTML source grabbed by the scraper contains a product available for purchase with B&H
-        :return: 0 if the product is not found available for purchase.
-        :return: 1 if the product is found and available for purchase.
+        Searches for whether or not the HTML source grabbed by the scraper contains a product available for purchase
+        with B&H :return: 0 if the product is not found available for purchase. :return: 1 if the product is found
+        and available for purchase.
         """
         results = self.soup.find(class_='cartRow_2dS2mdogHYAqhmKoANr6Ol')
-        if results != None:
+        if results is not None:
             if re.search("Add to Cart", str(results)):
                 webbrowser.open_new(URL)
                 return 1
         return 0
 
-    def getNE(self):
+    def getne(self):
         """
-        Searches for whether or not the HTML source grabbed by the scraper contains a product available for purchase with Newegg
-        :return: 0 if the product is not found available for purchase.
-        :return: 1 if the product is found and available for purchase.
+        Searches for whether or not the HTML source grabbed by the scraper contains a product available for purchase
+        with Newegg :return: 0 if the product is not found available for purchase. :return: 1 if the product is found
+        and available for purchase.
         """
         results = self.soup.find(id='ProductBuy')
-        if results == None:
+        if results is None:
             return 0
         themessage = results.find_all('button', class_='btn btn-primary btn-wide')
-        if themessage == None:
+        if themessage is None:
             for themessage in themessage:
                 if 'Add' in themessage.text:
                     webbrowser.open_new(URL)
                     return 1
         return 0
 
+
 URL = sys.argv[1]
 uType = sys.argv[2]
 sc = scraper(URL, uType)
 if __name__ == '__main__':
     if uType == 'BB':
-        exit( sc.getBB())
+        exit(sc.getbb())
     elif uType == 'NE':
-        exit(sc.getNE())
+        exit(sc.getne())
     elif uType == 'BH':
-        exit(sc.getBH())
+        exit(sc.getbh())
     else:
         print("Invalid type")
         exit(0)
