b74b5d9b78fa4b601d70808b2ae3b4a416c8e5d6
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-07-12 18:50:18 -0500

    Update initiator.py
    updated to pep8. Not all functionality was tested, consider unstable.

--- initiator.py
+++ initiator.py
@@ -14,41 +14,39 @@ class initiator:
     """
     This class handles the instantiation of a productList and has the ability to test sites and products
     """
-    def __init__(self, theProducts):
-        """""
-        Instantiates a class of type 'initiator'
-        :param theProducts: is the filename of where the properly formatted JSON file containing URL information is located
-        """""
-        self.prodLink = theProducts
-        defaultBrowser = ''
-        self.setDefaultBrowser()
+
+    def __init__(self, the_products):
+        """"" Instantiates a class of type 'initiator' :param the_products: is the filename of where the properly
+        formatted JSON file containing URL information is located "" """
+        self.prodLink = the_products
+        default_browser = ''
+        self.set_default_browser()
         self.data = None
         print(self.prodLink)
 
-    def setDefaultBrowser(self):
+    def set_default_browser(self):
         """
         Ensures that the default browser is set.
         :return: The first line in the file that is opened (which is the default browser)
         """
         try:
             with open('defaultBrowser.txt') as fp:
-                self.defaultBrowser = fp.readline()
+                self.default_browser = fp.readline()
         except:
             print("The 'defaultBrowser.txt' file could not be found")
 
-
-
-    def pollSite(self,baseURL):
+    @staticmethod
+    def poll_site(base_url):
         """
         Pings a website to see if the website is 'up'
-        :param baseURL: The website to be pinged
+        :param base_url: The website to be pinged
         :return: The number of successful pings to the target website
         """
-        host = re.search(r'//(.*?)/',baseURL)
-        if host != None:
+        host = re.search(r'//(.*?)/', base_url)
+        if host is not None:
             host = host.group(1)
         else:
-            host = baseURL
+            host = base_url
         print("Checking if the Host is up! ")
         # Option for the number of packets as a function of
         p_type = '-n' if platform.system().lower() == 'windows' else '-c'
@@ -74,50 +72,53 @@ class initiator:
             print("The JSON file could either not be found, or not be opened. Please check that it exists.")
             return 0
         print('Checking JSON content.')
-        if not 'Product' in self.data:
+        if 'Product' not in self.data:
             print("Invalid JSON, or the object is empty")
             return 0
-        theProcesses=[]
-        theData = []
-        numCrawled = 0
+        the_processes = []
+        the_data = []
+        num_crawled = 0
         for URL in self.data['Product']:
 
-            URLstr=str(URL['productLink'])
-            print('running for ' + URLstr)
-            if re.search("www.bestbuy.com/", URLstr) and self.pollSite("www.bestbuy.com"):
+            url_str = str(URL['productLink'])
+            print('running for ' + url_str)
+            if re.search("www.bestbuy.com/", url_str) and self.poll_site("www.bestbuy.com"):
+                the_processes.append(
+                    subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BB', self.default_browser]))
+                the_data.append([URL['productType'], url_str, URL['productPrice'], False])
+            if re.search("www.newegg.com/", url_str) and self.poll_site("www.newegg.com"):
+                the_processes.append(
+                    subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'NE', self.default_browser]))
+                the_data.append([URL['productType'], url_str, URL['productPrice'], False])
+            if re.search("www.bhphotovideo.com/", url_str) and self.poll_site("www.newegg.com"):
+                the_processes.append(
+                    subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BH', self.default_browser]))
+                the_data.append([URL['productType'], url_str, URL['productPrice'], False])
 
-                theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BB', self.defaultBrowser]))
-                theData.append([URL['productType'],URLstr,URL['productPrice'],False])
-            if re.search("www.newegg.com/", URLstr) and self.pollSite("www.newegg.com"):
-                theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'NE', self.defaultBrowser]))
-                theData.append([URL['productType'], URLstr, URL['productPrice'], False])
-            if re.search("www.bhphotovideo.com/", URLstr) and self.pollSite("www.newegg.com"):
-                theProcesses.append(subprocess.Popen(["python3", "scraper.py", URL['productLink'], 'BH', self.defaultBrowser]))
-                theData.append([URL['productType'], URLstr, URL['productPrice'], False])
-
-            while len(theProcesses) > 2:
-                count = 0 
-                for p in theProcesses :
+            while len(the_processes) > 2:
+                count = 0
+                for p in the_processes:
                     if p.poll() == 0:
-                        theData.pop(count)
-                        theProcesses.pop(count)
-                        numCrawled = numCrawled + 1
+                        the_data.pop(count)
+                        the_processes.pop(count)
+                        num_crawled = num_crawled + 1
                     elif p.poll() == 1:
-                        theProcesses.pop(count)
-                        interface.notification(theData[count][0],theData[count][1],theData[count][2],theData[count][3])
-                        theData.pop(count)
+                        the_processes.pop(count)
+                        interface.notification(the_data[count][0], the_data[count][1], the_data[count][2],
+                                               the_data[count][3])
+                        the_data.pop(count)
                         print('Interface call!!!!!')
-                        numCrawled = numCrawled + 1
+                        num_crawled = num_crawled + 1
                     elif p.poll() == 2:
-                        theProcesses.pop(count)
-                        theData.pop(count)
+                        the_processes.pop(count)
+                        the_data.pop(count)
                         print('Network Error')
-                        numCrawled = numCrawled + 1
+                        num_crawled = num_crawled + 1
                     count = count + 1
-        return numCrawled
+        return num_crawled
 
 
 if __name__ == '__main__':
-    gotime = initiator(theProducts="productList.json")
+    gotime = initiator(the_products="productList.json")
     print(gotime.initiate())
     exit()
