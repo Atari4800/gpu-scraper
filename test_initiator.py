244745dd98f99effcb735626c3f7551f8e04a261
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-07-13 14:04:42 -0500

    test additions
    Added more tests, they all pass.

--- test_initiator.py
+++ test_initiator.py
@@ -1,20 +1,49 @@
-import initiator
+import initiator as it
 import pytest
 
-test_obj = initiator()
+
 
 def test_init():
-    assert test_obj.prodLink == 'productList.json'
-    assert test_obj.defaultBrowser == ''
+    test_obj = it.initiator(theProducts="control_productList.json")
+    assert test_obj.prodLink == 'control_productList.json'
+    assert test_obj.defaultBrowser == 'firefox.desktop'
     assert test_obj.data == None
 
 def test_setDefaultBrowser():
+    test_obj = it.initiator(theProducts="control_productList.json")
     test_obj.setDefaultBrowser()
     assert test_obj.defaultBrowser == "firefox.desktop"
-    
-def test_pollSite():
-    result = test_obj.pollSite()
-    assert result.returncode == 0
 
-def test_initiate():
+def test_pollSiteIP():
+    test_obj = it.initiator(theProducts="control_productList.json")
+    assert test_obj.pollSite("8.8.8.8") == True
+def test_pollSitePartialURL():
+    test_obj = it.initiator(theProducts="control_productList.json")
+    assert test_obj.pollSite("www.google.com") == True
+def test_pollSiteFullURL():
+    test_obj = it.initiator(theProducts="control_productList.json")
+    assert test_obj.pollSite("https://www.google.com/") == True
+def test_pollSiteBadURL():
+    test_obj = it.initiator(theProducts="control_productList.json")
+    assert test_obj.pollSite("https://www.23971gsadbhefasdWQ@.com/") == False
+def test_pollSiteEmpty():
+    test_obj = it.initiator(theProducts="control_productList.json")
+    assert test_obj.pollSite("") == False
+def test_initiateNoJSON():
+    test_obj = it.initiator(theProducts="")
+    assert test_obj.initiate() == 0
+def test_initiateMalformedJSON():
+    test_obj = it.initiator(theProducts="control_productList.js")
+    assert test_obj.initiate()==0
+def test_initiateWrongJSONFile():
+    test_obj = it.initiator(theProducts="control_bad_productList.js")
+    assert test_obj.initiate()==0
+def test_initiateEmptyJSON():
+    test_obj = it.initiator(theProducts="control_bad_productList.js")
+    assert test_obj.initiate()==0
+def test_initiateProductsExist():
+    test_obj = it.initiator(theProducts="control_productList.json")
+    assert test_obj.initiate()== 8
+
+
 
