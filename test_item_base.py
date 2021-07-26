e128583fd4403aa46453d2b0f734caa86cc432fc
Harrison Koll <70385098+HKollU@users.noreply.github.com>
2021-07-26 12:01:24 -0500

    Forgot the test-itemBase.py push.
    I forgot to upload the test for itemBase.py
    
    Still need to refactor the adding tests so the data is not easy to read, but easy for the program to understand. (how JSON saves looks different/messier than how it is/should be written.

--- test_item_base.py
+++ test_item_base.py
@@ -18,37 +18,37 @@ test_json = 'test_productList.json'
 
 def test_addDuplicateItem():
     test_obj = ib.item_base()
-    assert test_obj.add_item(dupURL, json_file=test_json) == -5
+    assert test_obj.add_item(dupURL, None, None, test_json) == -5
 
 def test_addUnsupported():
     test_obj = ib.item_base()
-    assert test_obj.add_item(unsupURL, json_file=test_json) == -4
+    assert test_obj.add_item(unsupURL, None, None, test_json) == -4
 
 def test_addUnreachableDomain():
     test_obj = ib.item_base()
-    assert test_obj.add_item(unreaDomain, json_file=test_json) == -3
+    assert test_obj.add_item(unreaDomain, None, None, test_json) == -3
 
 def test_addBadJSON():
     test_obj = ib.item_base()
-    assert test_obj.add_item(urlBB, json_file=problemJSON) == -2
+    assert test_obj.add_item(urlBB, None, None, problemJSON) == -2
 
 def test_additemNotFound():
     test_obj = ib.item_base()
-    assert test_obj.add_item(itemNotFound, json_file=test_json) == -1
+    assert test_obj.add_item(itemNotFound, None, None, test_json) == -1
 
 def test_addJSONWriteError():
     test_obj = ib.item_base()
-    assert test_obj.add_item(urlBB, json_file=jsonNoWrite) == 0
-"""
+    assert test_obj.add_item(urlBB, None, None, jsonNoWrite) == 0
+
 def test_add_itemBB():
     test_obj = ib.item_base()
-    assert test_obj.add_item(urlBB, json_file=test_json)
+    assert test_obj.add_item(urlBB, None, None, json_file=test_json)
     with open(test_json, 'r') as test_file:
         test_data = json.load(test_file)
         with open(control_json, 'r') as control_file:
             control_data = json.load(control_file)
             assert test_data == control_data
-
+"""
 def test_DeleteItemBH():
     test_obj = ib.item_base()
     assert test_obj.delItem(urlBH, json_file=test_json) == 1
